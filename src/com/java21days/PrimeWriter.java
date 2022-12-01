c0d4617f5d359b60ca62e4d93c74bac2bd9ebce3
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 21:36:39 -0500

    Test isPrime method

--- src/com/java21days/PrimeWriter.java
+++ src/com/java21days/PrimeWriter.java
@@ -2,6 +2,14 @@ package com.java21days;
 
 public class PrimeWriter {
 
+    PrimeWriter(int numOfPrimes){
+        int num = 2;
+        for(int i=0; i<=numOfPrimes; i++){
+            if(isPrime(num)){
+                System.out.println(num);
+            }
+        }
+    }
 
     boolean isPrime(int num){
         double root = Math.sqrt(num);
