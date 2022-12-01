c1490cb76b9a45e725e5f34ea7ca489307668a32
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 21:33:47 -0500

    Add method to check if number is prime

--- src/com/java21days/PrimeWriter.java
+++ src/com/java21days/PrimeWriter.java
@@ -1,4 +1,15 @@
 package com.java21days;
 
 public class PrimeWriter {
+
+
+    boolean isPrime(int num){
+        double root = Math.sqrt(num);
+        for(int i=2; i<=root; i++){
+            if(num % i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
